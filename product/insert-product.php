9efe66473eb4dccfd9de3c0f821b42bc8a847796
kyxx <68198922+kyxx00@users.noreply.github.com>
2025-09-16 09:08:54 +0200

    Update insert-product.php

--- product/insert-product.php
+++ product/insert-product.php
@@ -1,19 +1,5 @@
 <?php
-require_once '../includes/user-class.php';
-
-class Product {
-    private $dbh;
-
-    public function __construct() {
-        $this->dbh = new DB();
-    }
-
-public function insertProduct($omschrijving, $foto, $prijs) {
-    $sql = "INSERT INTO product (omschrijving, foto, prijsPerStuk) 
-            VALUES (:omschrijving, :foto, :prijs)";
-    return $this->dbh->run($sql, [":omschrijving" => $omschrijving,":foto" => $foto,":prijs" => $prijs]);
-}
-}
+require_once '../includes/product-class.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
     $omschrijving = $_POST['omschrijving'];

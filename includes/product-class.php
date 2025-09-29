1edc991059194318fb1e23abb0609ad0a1d8d9b2
kyxx <68198922+kyxx00@users.noreply.github.com>
2025-09-29 13:24:06 +0200

    Update product-class.php

--- includes/product-class.php
+++ includes/product-class.php
@@ -14,5 +14,10 @@ public function insertProduct($omschrijving, $foto, $prijs) {
             VALUES (:omschrijving, :foto, :prijs)";
     return $this->dbh->run($sql, [":omschrijving" => $omschrijving,":foto" => $foto,":prijs" => $prijs]);
 }
+
+public function getAllProducts() {
+    $sql = "SELECT productId AS id, omschrijving, foto, prijsPerStuk FROM product";
+    return $this->dbh->run($sql)->fetchAll(PDO::FETCH_ASSOC);
+}
 }
 ?>
